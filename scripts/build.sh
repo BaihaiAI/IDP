2ff806744b6aa6ab4afa7108f2ea91abe2b652e7
jiangshanwen <jsw@baihai.ai>
2022-09-06 17:49:02 +0800

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,6 @@
 set -xue
 
-git submodule update --init --recursive
 pushd web
-git submodule update --init --recursive
-git checkout dev
-cd extension
-git checkout open
-cd ..
 # In mac M1 if nodejs version > 16, then require CXXFLAGS set to C++ 17
 yarn install && CXXFLAGS="--std=c++17" yarn install:terminal && yarn build:open
 popd
