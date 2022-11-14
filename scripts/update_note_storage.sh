89383006c835f0289e1691e8c37f3489b42126ae
wuaoxiang <os.popen@gmail.com>
2022-11-14 19:38:04 +0800

    add execute permission to scripts

--- scripts/update_note_storage.sh
+++ scripts/update_note_storage.sh
@@ -9,13 +9,18 @@ cd $repo_root
 binary=note_storage
 if [ $target == "musl" ]; then
 cargo b --bin $binary --target x86_64-unknown-linux-musl
-scp -C target/x86_64-unknown-linux-musl/debug/$binary $remote:/root/
+cp target/x86_64-unknown-linux-musl/debug/$binary target/$binary
 else
 cargo b --bin $binary
-scp -C target/debug/$binary $remote:/root/
+cp target/debug/$binary target/$binary
+fi
+local_binary_md5=$(md5sum target/$binary | cut -f 1 -d " ")
+remote_binary_md5=$(ssh $remote md5sum /$binary | cut -f 1 -d " ")
+if [ $local_binary_md5 != $remote_binary_md5 ]; then
+    scp -C target/$binary $remote:/$binary
 fi
 
 ssh $remote "kubectl -n $namespace exec $pod -- sudo supervisorctl stop $binary"
-ssh $remote "kubectl -n $namespace cp /root/$binary $pod:/tmp/$binary"
+ssh $remote "kubectl -n $namespace cp /$binary $pod:/tmp/$binary"
 ssh $remote "kubectl -n $namespace exec $pod -- sudo mv /tmp/$binary /usr/local/bin/"
 ssh $remote "kubectl -n $namespace exec $pod -- sudo supervisorctl restart $binary"
