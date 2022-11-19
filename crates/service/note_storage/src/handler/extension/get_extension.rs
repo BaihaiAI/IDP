8d8bff9c6f95d5a2b8c83035cb2e7e2bfffa04f4
huangyi <huangyi@baihai.ai>
2022-11-19 17:53:39 +0800

    update extionsion

--- crates/service/note_storage/src/handler/extension/get_extension.rs
+++ crates/service/note_storage/src/handler/extension/get_extension.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use super::get_extensions_config;
 const EXTENSION_HTTP_URL: &str = "http://baihai.cn-bj.ufileos.com/docker-build/extension-store/";
 const US3CLI_DEST: &str = "/home/ray/us3cli-linux64";
