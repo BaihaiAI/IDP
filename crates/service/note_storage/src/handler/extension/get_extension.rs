113f5f1c21741b1b0bd479b51ef3472cba1018a7
huangyi <huangyi@baihai.ai>
2022-11-19 15:03:27 +0800

    WIP

--- crates/service/note_storage/src/handler/extension/get_extension.rs
+++ crates/service/note_storage/src/handler/extension/get_extension.rs
@@ -1,28 +1,25 @@
 use super::get_extensions_config;
-const EXTENSION_DOWNLOAD_URL: &str = "http://baihai.cn-bj.ufileos.com/docker-build/extension-store/extensions_config.json";
+const EXTENSION_HTTP_URL: &str = "http://baihai.cn-bj.ufileos.com/docker-build/extension-store/";
 const US3CLI_DEST: &str = "/home/ray/us3cli-linux64";
+const US3_URL: &str = "us3://baihai/docker-build/extension-store/";
 
 pub async fn get_extension() {
     loop {
-        let extension_path = business::path_tool::recommended_extensions();
-        let extension_path = extension_path.join("extensions_config.json");
+        let store_path = business::path_tool::recommended_extensions();
+        let extension_path = store_path.join("extensions_config.json");
         let extension_resp = match get_extensions_config(&extension_path).await{
             Ok(data) => data,
             Err(err) => {tracing::error!("{}", err.to_string());continue},
         };
 
-        let parent_path = match extension_path.parent(){
-            Some(data) => data,
-            None => {tracing::error!("wrong extension path");continue},
-        };
-
-        let dest_path = parent_path.join("extension_temp.json");
+        let dest_path = store_path.join("extension_temp.json");
+        let origin_url = format!("{EXTENSION_HTTP_URL}extension_config.json");
         //curl -L -o /home/ray/extension-store/
         let mut cmd = tokio::process::Command::new("curl");
         cmd.arg("-L")
             .arg("-o")
             .arg(&dest_path)
-            .arg(EXTENSION_DOWNLOAD_URL);
+            .arg(&origin_url);
         let extension_resp_new = match cmd.spawn().expect("can't get current extension_config").wait().await{
             Ok(_) => {
                 match get_extensions_config(&dest_path).await{
@@ -45,7 +42,7 @@ pub async fn get_extension() {
 
             tracing::info!("resp_new: {:#?}",resp_new);
             tracing::info!("resp: {:#?}",resp);
-            if resp.is_none() || resp_new.cmp(&resp).is_lt(){
+            if resp.is_none() || (resp_new.cmp(&resp).is_lt()&&resp_new.is_some()){
                 tracing::info!("get_remote_extension");
                 let origin_name = match resp_new{
                     Some(data) => &data.name,
@@ -53,13 +50,13 @@ pub async fn get_extension() {
                 };
                 get_remote_extension(origin_name).await;
                 resp_new = resp_new_iter.next();
-            }else if resp_new.is_none() || resp_new.cmp(&resp).is_gt(){
+            }else if resp_new.is_none() || (resp_new.cmp(&resp).is_gt()&&resp.is_some()){
                 tracing::info!("remove extension");
-                let origin_name = match resp_new{
+                let origin_name = match resp{
                     Some(data) => &data.name,
                     None => "",
                 };
-                let remove_path = format!("{}/{}",EXTENSION_DOWNLOAD_URL,origin_name);
+                let remove_path = format!("{:#?}/{}",store_path,origin_name);
                 match tokio::fs::remove_dir_all(&remove_path).await{
                     Ok(_) => tracing::info!("successful remove extension: {:#?}",remove_path),
                     Err(err) => tracing::error!("fail to cp folder: {:#?},err:{:#?}",remove_path,err),
@@ -76,21 +73,22 @@ pub async fn get_extension() {
             tracing::error!("fail to overwrite past extension_config");
         };
 
-        tokio::time::sleep(std::time::Duration::from_secs(100)).await;
+        tokio::time::sleep(std::time::Duration::from_secs(500)).await;
     }
 }
 
 pub async fn get_remote_extension(name:&str){
-    let extension_path = business::path_tool::recommended_extensions();
-    let origin_path = format!("{}/{}",EXTENSION_DOWNLOAD_URL,name);
+    let store_path = business::path_tool::recommended_extensions();
+    let dest_path = format!("{:#?}/{}",store_path,name);
+    let origin_url = format!("{}{}",US3_URL,name);
     let mut cmd = tokio::process::Command::new(US3CLI_DEST);
     cmd.arg("cp")
         .arg("-r")
-        .arg(&origin_path)
-        .arg(&extension_path);
+        .arg(&origin_url)
+        .arg(&dest_path);
     match cmd.spawn().expect("can't get current extension_config").wait().await{
-        Ok(_) => tracing::info!("successful cp folder: {:#?} to destpath",origin_path),
-        Err(_) => tracing::info!("fail to cp folder: {:#?}",origin_path),
+        Ok(_) => tracing::info!("successful cp folder: {:#?} to destpath:{:#?}",origin_url,dest_path),
+        Err(_) => tracing::info!("fail to cp folder: {:#?} to destpath:{:#?}",origin_url,dest_path),
     }
 }
 
