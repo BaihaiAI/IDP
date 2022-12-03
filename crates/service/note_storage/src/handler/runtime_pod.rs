ce9fcd42cac9a9badec9df131707bb3afae7dcd2
wuaoxiang <wuaoxiang@baihai.ai>
2022-12-03 17:55:28 +0800

    feature: last_resource_setting

--- crates/service/note_storage/src/handler/runtime_pod.rs
+++ crates/service/note_storage/src/handler/runtime_pod.rs
@@ -15,21 +15,53 @@
 use axum::extract::Query;
 use common_model::Rsp;
 use err::ErrorTrace;
+use kernel_common::spawn_kernel_process::Resource;
+use tracing::warn;
 
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct ProjectId {
+pub struct Req {
     project_id: u64,
+    team_id: u64,
+}
+
+#[derive(serde::Serialize)]
+pub struct Resp {
+    running: bool,
+    resource: Resource,
 }
 
 #[allow(clippy::unused_async)]
 pub async fn runtime_pod_status(
-    Query(ProjectId { project_id }): Query<ProjectId>,
-) -> Result<Rsp<bool>, ErrorTrace> {
-    if !business::kubernetes::is_k8s() {
-        return Ok(Rsp::success(true));
-    }
-    Ok(Rsp::success(business::kubernetes::runtime_pod_is_running(
+    Query(Req {
         project_id,
-    )))
+        team_id,
+    }): Query<Req>,
+) -> Result<Rsp<Resp>, ErrorTrace> {
+    if !business::kubernetes::is_k8s() {
+        return Ok(Rsp::success(Resp {
+            running: true,
+            resource: Resource::default(),
+        }));
+    }
+    let team_id = 1;
+    let is_running = business::kubernetes::runtime_pod_is_running(project_id);
+    let path = format!("/store/{team_id}/projects/{project_id}/last_resource_setting.json");
+    let resource = match std::fs::read_to_string(&path) {
+        Ok(content) => match serde_json::from_str(&content) {
+            Ok(x) => x,
+            Err(err) => {
+                warn!("{path} {err}");
+                Resource::default()
+            }
+        },
+        Err(err) => {
+            warn!("{path} {err}");
+            Resource::default()
+        }
+    };
+    Ok(Rsp::success(Resp {
+        running: is_running,
+        resource,
+    }))
 }
